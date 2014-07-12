db1f97c00265783689f851576c77218c67f6f176
vhorta73 <diasfonseca@live.com>
2014-07-13 00:03:40 +0100

    Adding supplier sql schema

---
+++ schema/supplier.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `supplier` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` enum('Active','Inactive') NOT NULL DEFAULT 'Active',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `phone` varchar(255) NOT NULL DEFAULT '',
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_updated_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
