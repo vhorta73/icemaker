9dc14217f14c5f425c86f1bd306f2ca7479c9f5e
vhorta73 <diasfonseca@live.com>
2014-07-13 00:20:34 +0100

    Adding order_recipe sql schema

---
+++ schema/order_recipe.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `order_recipe` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `recipe_id` int(11) NOT NULL,
+  `status` enum('saved','cancelled','ready','in progress','pending','completed','closed') NOT NULL DEFAULT 'saved',
+  `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_updated_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
