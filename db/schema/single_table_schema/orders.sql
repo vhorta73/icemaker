56b7e92c777ace8bdc071b1c8f7b5ff1be7819a5
vhorta73 <diasfonseca@live.com>
2014-07-23 13:33:08 +0100

    Fixing orders table

--- db/schema/single_table_schema/orders.sql
+++ db/schema/single_table_schema/orders.sql
@@ -1,9 +1,12 @@
 -- Main orders table, linked to the customer ordering.
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `customer_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL DEFAULT '0',
   `status` enum('saved','cancelled','queued','in progress','pending','completed','closed') NOT NULL DEFAULT 'saved',
   `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_updated_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY(`customer_id`),
+  KEY(`status`),
+  KEY(`creation_date`)
 );
