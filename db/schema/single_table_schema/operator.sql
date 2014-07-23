35d455e1362da57fe96388eb2fe6081c6511104d
vhorta73 <diasfonseca@live.com>
2014-07-23 14:25:09 +0100

    Adding sql table for operator and DBIx::Class model

---
+++ db/schema/single_table_schema/operator.sql
@@ -0,0 +1,13 @@
+-- Operator table, for employees operating machines.
+CREATE TABLE `operator` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL DEFAULT '',
+  `last_name` varchar(255) NOT NULL DEFAULT '',
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_updated_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY(`first_name`),
+  KEY(`last_name`),
+  KEY(`creation_date`)
+);
